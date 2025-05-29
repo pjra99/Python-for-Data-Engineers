9429064acfe7771c0e05d2cbe647c3d829934a54
Pranjal Rai <raipranjalofficial@gmail.com>
2025-05-30 02:12:28 +0530

    --added: some content in the last file

--- 5.SetIndexAndHandleNA.py
+++ 5.SetIndexAndHandleNA.py
@@ -5,7 +5,7 @@ def main():
             (102, 'Priya', 'HR', 60000, 'Mumbai'),
             (103, 'Anil', 'IT', None, 'Hyderabad'),
             (104, 'Sneha', None, 62000, 'Pune'),
-            (105, 'Manish', 'Finance', 90000, 'Chennai'),
+            (None, 'Manish', 'Finance', 90000, 'Chennai'),
             (106, 'Suresh', 'IT', 78000, 'Bangalore')]
     
     columns = ["EmpID", "Name", "Department", "Salary", "Location"]
@@ -16,11 +16,29 @@ def main():
     #attribute, or we set inplace attribute to false, like in below example
     df_ = df.set_index("EmpID")
 
-    print(df_)
+    # print(df_)
 
     #If we set inplace attribute to True, that means that the original df is modified
     df.set_index("EmpID", inplace=True)
+
+    #Replacing NA values using fillna, values inside NA function is the replacement value, like shown in below examples
+    df1 = df.fillna('Not Avl')
+    df2 = df.fillna(0)
+
+    #We can also pass a dictionary in fillna function, as a param, where the key is the column name and the value is the 
+    #replacement value for that column, see the below ex
+    df3 = df.fillna({
+        "EmpID":0,
+        "Department":"Dep to be assigned yet",
+        "Salary":0,
+        "Location":"Not Avl"
+    })
+    #In the below output, EmpID value won't be replaced because indexed columns don't accepted a replacement value as that's
+    #the whole purpose of index, right?
+    print(df3)
+
     
+
 
 
 
